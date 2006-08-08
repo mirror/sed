b8eb0a13d4c3031d33220da3c6a855d5dc8446d1
Paolo Bonzini <bonzini@gnu.org>
2006-08-08 10:29:48 +0000

    fix cygwin binary mounts
    2006-08-03  Paolo Bonzini  <bonzini@gnu.org>
    
    	Revert this part of the previous change:
    
    	2006-08-03  Paolo Bonzini  <bonzini@gnu.org>
    
    	* sed/compile.c (get_openfile): Change second argument to char
    	and turn it into a string within the function.  Adjust callers.
    
    git-archimport-id: bonzini@gnu.org--2004b/sed--stable--4.1--patch-72

--- sed/compile.c
+++ sed/compile.c
@@ -374,16 +374,15 @@ read_filename()
   return b;
 }
 
-static struct output *get_openfile P_((struct output **file_ptrs, char mode, bool fail));
+static struct output *get_openfile P_((struct output **file_ptrs, const char *mode, bool fail));
 static struct output *
 get_openfile(file_ptrs, mode, fail)
      struct output **file_ptrs;
-     char mode;
+     const char *mode;
      bool fail;
 {
   struct buffer *b;
   char *file_name;
-  char file_mode[3];
   struct output *p;
   int is_stderr;
 
@@ -412,13 +411,9 @@ get_openfile(file_ptrs, mode, fail)
 
   if (!p)
     {
-      file_mode[0] = mode;
-      file_mode[1] = read_mode[1];
-      file_mode[2] = '\0';
-
       p = OB_MALLOC(&obs, 1, struct output);
       p->name = ck_strdup(file_name);
-      p->fp = ck_fopen(p->name, file_mode, fail);
+      p->fp = ck_fopen(p->name, mode, fail);
       p->missing_newline = false;
       p->link = *file_ptrs;
       *file_ptrs = p;
@@ -651,7 +646,7 @@ mark_subst_opts(cmd)
 	break;
 
       case 'w':
-	cmd->outf = get_openfile(&file_write, 'w', true);
+	cmd->outf = get_openfile(&file_write, "w", true);
 	return flags;
 
       case '0': case '1': case '2': case '3': case '4':
@@ -1239,12 +1234,12 @@ compile_program(vector)
 	  break;
 
         case 'R':
-	  cur_cmd->x.fp = get_openfile(&file_read, 'r', false)->fp;
+	  cur_cmd->x.fp = get_openfile(&file_read, read_mode, false)->fp;
 	  break;
 
         case 'W':
 	case 'w':
-	  cur_cmd->x.outf = get_openfile(&file_write, 'w', true);
+	  cur_cmd->x.outf = get_openfile(&file_write, "w", true);
 	  break;
 
 	case 's':
