d7df092736733fd9d1f28c1731dc17535d23ec9d
Paolo Bonzini <bonzini@gnu.org>
2006-12-21 09:15:47 +0000

    include string.h where necessary
    2006-12-21  Paolo Bonzini  <bonzini@gnu.org>
    
    	* sed/mbcs.c: Include string.h (reported by Henning Nielsen Lund).
    	* sed/fmt.c: Likewise.
    	* sed/regexp.c: Likewise.
    
    git-archimport-id: bonzini@gnu.org--2004b/sed--stable--4.1--patch-89

--- sed/fmt.c
+++ sed/fmt.c
@@ -20,6 +20,7 @@
 #include "sed.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 #include <sys/types.h>
 
--- sed/mbcs.c
+++ sed/mbcs.c
@@ -17,6 +17,7 @@
 
 #include "sed.h"
 #include <stdlib.h>
+#include <string.h>
 
 #ifdef HAVE_LANGINFO_CODESET
 #include <langinfo.h>
