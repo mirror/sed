3939f507a2a8bedcf25dc01d4f17e546fda1f375
Assaf Gordon <assafgordon@gmail.com>
2016-05-26 20:35:59 -0400

    tests: test invalid backreferences in gnu/posix modes
    's/foo/\1/' is rejected by default (gnu extension),
    silently accepted in POSIX.
    
    * testsuite/posix-mode-bad-ref.sh: new test.
    * testsuite/Makefile.am: add new test.

---
+++ testsuite/posix-mode-bad-ref.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Test non-posix-conforming gnu extensions when using --posix.
+
+# Copyright (C) 2016 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+. "${srcdir=.}/init.sh"; path_prepend_ ../sed
+print_ver_ sed
+
+fail=0
+
+cat <<\EOF >exp-err || framework_failure_
+sed: -e expression #1, char 10: invalid reference \1 on `s' command's RHS
+EOF
+
+# Invalid references are errors in non-posix mode
+returns_ 1 sed 's/abc/\1/g' 2>err < /dev/null || fail=1
+compare_ exp-err err || fail=1
+
+# Invalid referencs are silently ignored in posix mode
+sed --posix 's/abc/\1/g' < /dev/null || fail=1
+
+Exit $fail
