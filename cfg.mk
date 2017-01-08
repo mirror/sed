2a6eccb7eebfbcfc9c499453c5ce9768c02589d5
Jim Meyering <meyering@fb.com>
2017-01-07 16:43:41 -0800

    doc: fix NEWS typo
    * NEWS: Fix typo s/introducted/introduced/
    * cfg.mk (old_NEWS_hash): Update.
    Reported by Karl Berry.

--- cfg.mk
+++ cfg.mk
@@ -68,7 +68,7 @@ export VERBOSE = yes
 # 836832 6e
 export XZ_OPT = -6e
 
-old_NEWS_hash = 2c25279ade6c4c6d4f7a465d5833aac6
+old_NEWS_hash = bf769101cd387fc7a69a6f364fdb532d
 
 # Many m4 macros names once began with 'jm_'.
 # Make sure that none are inadvertently reintroduced.
