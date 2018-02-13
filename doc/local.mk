a2daab7fb15dc1518032b788e9e4c75ac00f4c44
Assaf Gordon <assafgordon@gmail.com>
2018-02-13 16:02:12 -0700

    maint: change http to https in license headers
    * all files: Change http to https for all licenses URL.

--- doc/local.mk
+++ doc/local.mk
@@ -11,7 +11,7 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 info_TEXINFOS = doc/sed.texi
 doc_sed_TEXINFOS = doc/config.texi doc/version.texi doc/fdl.texi
--- testsuite/colon-with-no-label.sh
+++ testsuite/colon-with-no-label.sh
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 . "${srcdir=.}/testsuite/init.sh"; path_prepend_ ./sed
 print_ver_ sed
 
