36e24f199f32ba51796f0cd4da690674059ee91a
Jim Meyering <meyering@fb.com>
2020-01-20 17:05:45 -0800

    build: update to newer help2man
    * build-aux/help2man: Update from coreutils.
    This updates from 1.28 to 1.47.3
    * cfg.mk (exclude_file_name_regexp--sc_long_lines):
    Exempt help2man from the 80-column-max limit.
    This addresses https://bugs.gnu.org/30471

--- cfg.mk
+++ cfg.mk
@@ -328,7 +328,7 @@ exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = \
   ^testsuite/(bkslashes.good|(noeolw?|empty|zero-anchor)\.(2?good|inp))$$
 
 # Exempt test-related files from our 80-column limitation, for now.
-exclude_file_name_regexp--sc_long_lines = ^testsuite/
+exclude_file_name_regexp--sc_long_lines = (^testsuite/|/help2man$$)
 
 # Exempt test-related files from space-before-parens requirements.
 exclude_file_name_regexp--sc_space_before_open_paren = ^testsuite/
