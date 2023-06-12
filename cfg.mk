f4ac1e793131479e8c4026fc0b7133cc73798ade
Jim Meyering <meyering@meta.com>
2023-06-12 08:29:31 -0700

    maint: long_lines vs bootstrap: avoid syntax-check failure
    * cfg.mk (exclude_file_name_regexp--sc_long_lines):
    Exempt generated bootstrap from line-length check.

--- cfg.mk
+++ cfg.mk
@@ -330,7 +330,8 @@ exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = \
   ^testsuite/(bkslashes.good|(noeolw?|empty|zero-anchor)\.(2?good|inp))$$
 
 # Exempt test-related files from our 80-column limitation, for now.
-exclude_file_name_regexp--sc_long_lines = (^testsuite/|/help2man$$)
+exclude_file_name_regexp--sc_long_lines = \
+  (^testsuite/|/help2man$$|^bootstrap$$)
 
 # Exempt test-related files from space-before-parens requirements.
 exclude_file_name_regexp--sc_space_before_open_paren = ^testsuite/
